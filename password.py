ca161f722e36d91aa0557621189d8c5786c8c521
Lateworm <travis.rempel@gmail.com>
2019-11-09 09:59:38 -0800

    move all game data into a context

--- password.py
+++ password.py
@@ -3,9 +3,10 @@ from pygame.locals import *
 from time import sleep
 import random
 
-# CONSTANTS
+# CONTEXT
 
-cfg = {
+ctx = {
+  # CONFIG
   'font_face': 'couriernew',
   'font_height': 18,
   'line_height': 20,
@@ -15,52 +16,47 @@ cfg = {
   'colour_bg': (46, 46, 46),
   'colour_text': (126, 224, 126),
   'colour_alpha': (86, 135, 86), # TODO: extract this to a function
+
+  # CONTENT
+  'word_sets': [
+    [
+      'PUTTING', 'CUTTING',            # _UTTING
+      'HUNTING', 'SETTING',            # ___TING
+      'HEARING', 'NOTHING', 'FINDING', # ____ING
+      'PROVIDE', 'SURVIVE', 'REALIZE', # ____I_E
+      'CANTINA', 'HUNTERS',            # __NT___
+      'OVERLAP',
+    ], [
+      'SAILING', 'BAILING', # _AILING
+      'GRAVELY', 'BRAVELY', # _RAVELY
+      'TACKING', 'SINKING', # ___KING            
+      'NETTING', 'BETTING', # _ETTING
+      'WILDCAT', 'TARGETS',
+      'HUNTING',
+      'RAISINS', 'WETSUIT'
+    ]
+  ],
+  'decoy_chars': '1234567890QWERTYUIOPASDFGHJKLZXCVBNM`~!@#$%^&*()-_=+[{]}\|;:,<.>/?',
+  'attempts': 4,
 }
 
-word_sets = [
-  [
-    'PUTTING', 'CUTTING',            # _UTTING
-    'HUNTING', 'SETTING',            # ___TING
-    'HEARING', 'NOTHING', 'FINDING', # ____ING
-    'PROVIDE', 'SURVIVE', 'REALIZE', # ____I_E
-    'CANTINA', 'HUNTERS',            # __NT___
-    'OVERLAP',
-  ], [
-    'SAILING', 'BAILING', # _AILING
-    'GRAVELY', 'BRAVELY', # _RAVELY
-    'TACKING', 'SINKING', # ___KING            
-    'NETTING', 'BETTING', # _ETTING
-    'WILDCAT', 'TARGETS',
-    'HUNTING',
-    'RAISINS', 'WETSUIT'
-  ]
-]
-
-decoy_chars = [
-  # TODO: store this as a string
-  '`', '~', '!', '#', '$', '%', '^', '*', '(', ')', '-', '_', '+', '=',
-  '{', '}', '[', ']', '|',
-  ':', ';', '"', "'",
-  ',', '<', '.', '>', '/', '?',
-]
-
-words = word_sets[random.randint(0, len(word_sets)-1)]
-random.shuffle(words)
-password = words[random.randint(0, len(words)-1)]
 
 # SETUP
 
-attempts = 4
+ctx['words'] = ctx['word_sets'][random.randint(0, len(ctx['word_sets'])-1)]
+random.shuffle(ctx['words'])
+ctx['password'] = ctx['words'][random.randint(0, len(ctx['words'])-1)]
+
 
 pygame.init()
 clock = pygame.time.Clock()
-pygame.display.set_caption(cfg['window_title'])
+pygame.display.set_caption(ctx['window_title'])
 
-window = pygame.display.set_mode([cfg['line_height'] * cfg['window_width'], cfg['line_height'] * cfg['window_height']])
-window.fill(cfg['colour_bg'])
+window = pygame.display.set_mode([ctx['line_height'] * ctx['window_width'], ctx['line_height'] * ctx['window_height']])
+window.fill(ctx['colour_bg'])
 
 # TODO: is there some way to package a font so we can get it to render the same cross-platform?
-font = pygame.font.SysFont(cfg['font_face'], cfg['font_height'])
+font = pygame.font.SysFont(ctx['font_face'], ctx['font_height'])
 # font.set_bold(True)
 
 
@@ -68,24 +64,25 @@ font = pygame.font.SysFont(cfg['font_face'], cfg['font_height'])
 
 def line_offset(px):
   if px == 'center':
-    return cfg['line_height']
+    return ctx['line_height']
     # TODO: write centering logic
   else:
-    return cfg['line_height'] * px
+    return ctx['line_height'] * px
 
-def window_print(*, str, colour = cfg['colour_text'], x, y):
+def window_print(*, str, colour = ctx['colour_text'], x, y):
   window.blit(
-    font.render(str, True, colour, cfg['colour_bg']),
+    font.render(str, True, colour, ctx['colour_bg']),
     (line_offset(x), line_offset(y))
   )
   pygame.display.update()
+  # TODO: set up delayed printing
     
 def window_input(prompt, x, y):
   input_str = []
   delimiter = ''
   
   window.blit(
-    font.render(prompt + '_______', True, cfg['colour_text'], cfg['colour_bg']),
+    font.render(prompt + '_______', True, ctx['colour_text'], ctx['colour_bg']),
     (line_offset(x), line_offset(y))
   )
   pygame.display.update()
@@ -108,12 +105,12 @@ def window_input(prompt, x, y):
           
         display_line = prompt + display_str
         window.blit(
-          font.render(display_line, True, cfg['colour_text'], cfg['colour_bg']),
+          font.render(display_line, True, ctx['colour_text'], ctx['colour_bg']),
           (line_offset(x), line_offset(y))
         )
         pygame.display.update()
   
-  # TODO: avoid immediately returning on 7th character
+  # TODO: handle window exit in this loop
   return delimiter.join(input_str)
 # end window_input
 
@@ -136,7 +133,7 @@ def evaluate_guess(guess, password):
   return matching_positions
 # end evaluate_guess
 
-def handle_guess(attempts):
+def handle_guess(attempts, password):
   guess = window_input('ENTER PASSWORD > ', 1, 18+4-attempts)
   matching_positions = evaluate_guess(guess, password)
   
@@ -152,7 +149,7 @@ def handle_guess(attempts):
     window_print(str = "%s ATTEMPT(S) LEFT" %(attempts), x = 1, y = 2)
     
   if (guess == password):
-    window.fill(cfg['colour_bg'])
+    window.fill(ctx['colour_bg'])
     window_print(str = 'LOGIN SUCCESSFUL', x = 'center', y = 9)
     window_print(str = 'WELCOME BACK, COMMANDER', x = 'center', y = 11)
     window_print(str = 'PRESS ENTER TO EXIT', x = 'center', y = 13)
@@ -160,7 +157,7 @@ def handle_guess(attempts):
     return
   
   if (attempts == 0 and guess != password):
-    window.fill(cfg['colour_bg'])
+    window.fill(ctx['colour_bg'])
     window_print(str = 'LOGIN FAILURE', x = 'center', y = 9)
     window_print(str = 'INITIATING SYSTEM LOCKDOWN', x = 'center', y = 11)
     window_print(str = 'PRESS ENTER TO EXIT', x = 'center', y = 13)
@@ -168,16 +165,16 @@ def handle_guess(attempts):
     return
     
   if (attempts > 0):
-    handle_guess(attempts)
+    handle_guess(attempts, password)
 # end handle_guess
     
-    
+  
 # SCRIPT
 
 window_print(str = 'PASSWORD RECOVERY CONSOLE', x = 1, y = 1)
-window_print(str = "%s ATTEMPT(S) LEFT" %(attempts), x = 1, y = 2)
+window_print(str = "%s ATTEMPT(S) LEFT" %(ctx['attempts']), x = 1, y = 2)
 
-for num, word in enumerate(words, start = 4):
+for num, word in enumerate(ctx['words'], start = 4):
   decoy_length = 9
   total_length = 24
   prefix_length = random.randint(1, total_length - len(word) + 1)
@@ -185,20 +182,19 @@ for num, word in enumerate(words, start = 4):
   decoy_prefix = ''
 
   for i in range(0, total_length):
-    decoy_bg = decoy_bg + decoy_chars[random.randint(0, len(decoy_chars)-1)]
+    decoy_bg = decoy_bg + ctx['decoy_chars'][random.randint(0, len(ctx['decoy_chars'])-1)]
   for i in range(0, prefix_length):
     decoy_prefix = decoy_prefix + decoy_bg[i]
   
-  window_print(str = decoy_bg, colour = cfg['colour_alpha'], x = 1, y = num)
+  window_print(str = decoy_bg, colour = ctx['colour_alpha'], x = 1, y = num)
   window_print(str = decoy_prefix + word, x = 1, y = num)
-  window_print(str = decoy_prefix, colour = cfg['colour_alpha'], x = 1, y = num)
+  window_print(str = decoy_prefix, colour = ctx['colour_alpha'], x = 1, y = num)
   
 # guess = window_input(prompt = 'ENTER PASSWORD > ', x = 1, y = 18 + 4 - attempts)
-handle_guess(attempts)
+handle_guess(ctx['attempts'], ctx['password'])
 
 
 # Make the window closable
- 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
